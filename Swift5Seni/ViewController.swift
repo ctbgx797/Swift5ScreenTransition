1a0f4e6524e41e1b47364db64a07409b7226036e
Yoshiki <villager2525@gmail.com>
2020-11-25 19:16:32 +0900

    first commit

---
+++ Swift5Seni/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift5Seni
+//
+//  Created by 西谷恭紀 on 2020/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
